bee22a17af54781d24c2cf7e0d75687401c64b4d
Vinícius Moreira <vinicius.vsm777@gmail.com>
2021-09-30 00:49:47 -0300

    project base

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+const connection = require("./database/database");
+
+// View Engine
+app.set('view engine', 'ejs');
+
+// Using static files
+app.use(express.static('public'));
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com sucesso");
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(8080, () => {
+    console.log("O servidor está rodando!");
+});
