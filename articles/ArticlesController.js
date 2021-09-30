a0fab3dccf2e1fcdba3cd8bb9f23b547917f97fc
Vinícius Moreira <vinicius.vsm777@gmail.com>
2021-09-30 16:48:01 -0300

    Model Category created

---
+++ articles/ArticlesController.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/articles', (req, res) => {
+    res.send("ROTA DE ARTIGOS!");
+});
+router.get('/admin/articles/new', (req, res) => {
+    res.send("ROTA DE CRIAÇÃO DE ARTIGOS");
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-
 const connection = require("./database/database");
+
+const categoriesController = require("./categories/CategoriesController");
+const articlesController = require("./articles/ArticlesController");
 
 // View Engine
 app.set('view engine', 'ejs');
@@ -23,6 +25,9 @@ connection
     .catch((error) => {
         console.log(error);
     });
+
+app.use("/", categoriesController);
+app.use("/", articlesController);
 
 app.get("/", (req, res) => {
     res.render("index");
