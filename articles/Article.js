118dc5434287b3729740d48db76f5246150cecbf
Vinícius Moreira <vinicius.vsm777@gmail.com>
2021-10-02 00:47:29 -0300

    definindo relacionamentos no Sequelize

--- articles/Article.js
+++ articles/Article.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const connection = require("../database/database");
+const Category = require("../categories/Category");
 
 const Article = connection.define("articles", {
     title: {
@@ -15,5 +16,9 @@ const Article = connection.define("articles", {
         allowNull: false,
     }
 });
+
+Category.hasMany(Article); // Uma categoria tem muitos artigos (1-P-N)
+// `category_id` INT NOT NULL FOREIGN REFERENCES `category`
+Article.belongsTo(Category); // Um (1) Artigo (pertence) a uma (1) Categoria
 
 module.exports = Article;
